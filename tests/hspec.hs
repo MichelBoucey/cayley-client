a426f67a2ebb3bef88d4d245542cfebe9add4a46
Michel Boucey <michel.boucey@cybervisible.fr>
2016-09-13 15:25:35 +0200

    wip on tests

--- tests/hspec.hs
+++ tests/hspec.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.Aeson
-import Database.Cayley.Client as C
-import Data.HashMap.Strict
-import Test.Hspec
+import           Data.Aeson
+import           Data.HashMap.Strict
+import           Database.Cayley.Client as C
+import           Test.Hspec
 
 main :: IO ()
 main = hspec $ do
@@ -12,3 +12,15 @@ main = hspec $ do
         it "writes a quad to Cayley server" $ do
             c <- connectCayley defaultCayleyConfig
             write c Quad { subject = "danny", predicate = "follows", C.object = "sandy", label = Nothing } `shouldReturn` Just (Object (fromList [("result",String "Successfully wrote 1 quads.")]))
+
+    describe "query" $
+        it "query to Cayley server" $ do
+            c <- connectCayley defaultCayleyConfig
+            Right a <- query c "graph.Vertex('sandy').In('follows').All()"
+            encode a `shouldBe` "[{\"id\":\"danny\"}]"
+
+    describe "writeNQuadFile" $
+        it "writes quads from a file to Cayley server" $ do
+            c <- connectCayley defaultCayleyConfig
+            (writeNQuadFile c "./tests/testdata.nq" >>= successfulResults) `shouldReturn` Right 15
+
