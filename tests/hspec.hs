d0741b21548c9bfb96fb184624dfd55cabbb5a14
Michel Boucey <michel.boucey@cybervisible.fr>
2021-10-23 17:02:10 +0200

    Fixed for Aeson >= 2.0

--- tests/hspec.hs
+++ tests/hspec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP               #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 import           Data.Aeson
@@ -5,6 +6,10 @@ import           Data.HashMap.Strict
 import           Database.Cayley.Client as C
 import           Database.Cayley.Types  as CT
 import           Test.Hspec
+#if MIN_VERSION_aeson(2,0,0)
+import qualified Data.Aeson.Key       as K
+import qualified Data.Aeson.KeyMap    as KM
+#endif
 
 main :: IO ()
 main = hspec $ do
@@ -12,7 +17,11 @@ main = hspec $ do
     describe "write" $
         it "writes a quad to Cayley server" $ do
             c <- connectCayley defaultCayleyConfig
+#if MIN_VERSION_aeson(2,0,0)
+            write c Quad { subject = "<danny>", predicate = "<follows>", C.object = "<sandy>", label = Nothing } `shouldReturn` Just (Object (KM.fromList [("result",String "Successfully wrote 1 quads.")]))
+#else
             write c Quad { subject = "<danny>", predicate = "<follows>", C.object = "<sandy>", label = Nothing } `shouldReturn` Just (Object (fromList [("result",String "Successfully wrote 1 quads.")]))
+#endif
 
     describe "query" $
         it "query Cayley server" $ do
